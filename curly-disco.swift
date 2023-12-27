bcc2e883303c663f7d88796e29dc03bf85311e37
Sea Side Studios <150905026+valentinmarkin97@users.noreply.github.com>
2023-12-28 00:18:42 +0530

    Create curly-disco.swift

---
+++ curly-disco.swift
@@ -0,0 +1,5 @@
+ "treasure": Room(
+                "Treasure Room",
+                "You've found a room full of treasures! What do you do?",
+                ["Take treasures", "Leave"]
+            ),
